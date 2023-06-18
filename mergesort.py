2a975950687d4ce6c13ce5692750c19d18a879c8
Blufi02 <mope@harz.de>
2023-06-18 20:05:42 +0200

    cleanup mergsort.py

--- mergesort.py
+++ mergesort.py
@@ -1,8 +1,8 @@
-def ASSIGNMENT(new_list, i, old_list, j):
+def assignment(new_list, i, old_list, j):
     new_list[i] = old_list[j]
 
 
-def mergeSort(list_to_sort_by_merge):
+def merge_sort(list_to_sort_by_merge):
     if (
         len(list_to_sort_by_merge) > 1
         and not len(list_to_sort_by_merge) < 1
@@ -12,8 +12,8 @@ def mergeSort(list_to_sort_by_merge):
         left = list_to_sort_by_merge[:mid]
         right = list_to_sort_by_merge[mid:]
 
-        mergeSort(left)
-        mergeSort(right)
+        merge_sort(left)
+        merge_sort(right)
 
         l = 0
         r = 0
@@ -21,10 +21,10 @@ def mergeSort(list_to_sort_by_merge):
 
         while l < len(left) and r < len(right):
             if left[l] <= right[r]:
-                ASSIGNMENT(new_list=list_to_sort_by_merge, i=i, old_list=left, j=l)
+                assignment(new_list=list_to_sort_by_merge, i=i, old_list=left, j=l)
                 l += 1
             else:
-                ASSIGNMENT(new_list=list_to_sort_by_merge, i=i, old_list=right, j=r)
+                assignment(new_list=list_to_sort_by_merge, i=i, old_list=right, j=r)
                 r += 1
             i += 1
 
@@ -45,7 +45,8 @@ my_list = [54, 26, 93, 17, 77, 31, 44, 55, 20]
 x = range(len(my_list))
 plt.plot(x, my_list)
 plt.show()
-mergeSort(my_list)
+merge_sort(my_list)
+
 x = range(len(my_list))
 plt.plot(x, my_list)
 plt.show()
